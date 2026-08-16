8bd564e43cda22999fd64653a3372fb4e50061d4
Szamansaju <szamansaju@gmail.com>
2026-08-16 17:24:58 +0600

    Error Solved

---
+++ types/global.d.ts
@@ -0,0 +1,11 @@
+// src/types/global.d.ts
+import { Connection } from 'mongoose';
+
+declare global {
+    var mongoose: {
+        conn: Connection | null;
+        promise: Promise<Connection> | null;
+    };
+}
+
+export {};
